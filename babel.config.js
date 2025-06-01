fdef55fc347e00c7e19dcc53bc62b467f8cc2709
Jhonata Kornaker <jhonataalberto@outlook.com>
2025-06-01 08:16:36 -0300

    feat: criar variavel de ambiente env e configurando no projeto.

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel'],
+    plugins: [
+      // NativeWind deve vir primeiro ou antes do final
+      'nativewind/babel',
+
+      // Variáveis de ambiente
+      ['module:react-native-dotenv', {
+        moduleName: '@env',
+        path: '.env',
+        allowUndefined: true,
+      }],
+
+      // Alias de caminhos
+      ['module-resolver', {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      }],
+    ],
   };
 };
