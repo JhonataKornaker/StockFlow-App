cd8c14f3ecb402301eb9bf362802baa565f295f6
Jhonata Kornaker <jhonataalberto@outlook.com>
2025-06-03 07:36:39 -0300

    refactor: ajustar codigo e imports.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AppNavigation from './src/navigation';
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
+import AppNavigation from './src/navigation/AppNavigation';
 
 export default function App() {
-  return (
-    <AppNavigation/>
-  );
+  return <AppNavigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
