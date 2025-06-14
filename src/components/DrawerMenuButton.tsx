da83f2d406ebb68b0c78033e195e17f33adca8e8
Jhonata Kornaker <jhonataalberto@outlook.com>
2025-06-14 20:14:58 -0300

    refactor: ajustar botao do menu lateral.

--- src/components/DrawerMenuButton.tsx
+++ src/components/DrawerMenuButton.tsx
@@ -1,14 +1,13 @@
-import { TouchableOpacity } from "react-native";
-import { DrawerActions, useNavigation } from "@react-navigation/native";
-import { Menu } from "lucide-react-native";
+import { TouchableOpacity } from 'react-native';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
+import { Menu } from 'lucide-react-native';
 
 export default function DrawerMenuButton() {
   const navigation = useNavigation();
 
   return (
     <TouchableOpacity
-      onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
-      style={{ marginLeft: 15 }}
+      onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
     >
       <Menu size={24} color="#C5D4EB" />
     </TouchableOpacity>
