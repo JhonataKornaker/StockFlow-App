b857a18b61d2d8e1ea05302b3d8b2fe567ee580a
Jhonata Kornaker <jhonataalberto@outlook.com>
2025-06-21 17:19:09 -0300

    refactor: remover nativewind e adicionar novas dependecias para menu lateral.

--- babel.config.js
+++ babel.config.js
@@ -3,24 +3,27 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // NativeWind deve vir primeiro ou antes do final
-      'nativewind/babel',
-
       // Variáveis de ambiente
-      ['module:react-native-dotenv', {
-        moduleName: '@env',
-        path: '.env',
-        allowUndefined: true,
-      }],
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          allowUndefined: true,
+        },
+      ],
 
       // Alias de caminhos
-      ['module-resolver', {
-        root: ['./src'],
-        alias: {
-          '@': './src',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@': './src',
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
         },
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-      }],
+      ],
     ],
   };
 };
