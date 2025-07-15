55444cd0e7dfb3cbef0a594ed3b96ff343ed673e
Jhonata Kornaker <jhonataalberto@outlook.com>
2025-07-15 20:31:51 -0300

    refactor: ajustar componente ScreenCadastro.

--- src/components/ScreenCadastro.tsx
+++ src/components/ScreenCadastro.tsx
@@ -6,16 +6,18 @@ import { LucideIcon } from 'lucide-react-native';
 import { Button } from './Button';
 import { theme } from '@/styles/theme';
 
-interface ScreenCadastroProps {
+interface ScreenCadastroProps<T> {
   placeholders?: string[];
   icons?: (LucideIcon | undefined)[];
   iconColors?: string[];
   placeholderColors?: string[];
   children?: React.ReactNode;
   style?: React.CSSProperties;
+  defaultValues?: Partial<T>;
+  fieldKeys?: (keyof T)[];
 }
 
-export default function ScreenCadastro(props: ScreenCadastroProps) {
+export default function ScreenCadastro<T>(props: ScreenCadastroProps<T>) {
   const {
     placeholders = [],
     icons = [],
@@ -23,7 +25,10 @@ export default function ScreenCadastro(props: ScreenCadastroProps) {
     placeholderColors = [],
     children,
     style,
+    defaultValues,
+    fieldKeys = [],
   } = props;
+
   return (
     <Screen>
       <View
@@ -33,17 +38,26 @@ export default function ScreenCadastro(props: ScreenCadastroProps) {
           marginTop: 16,
         }}
       >
-        {placeholders.map((placeholder, index) => (
-          <Input
-            key={index}
-            placeholder={placeholder || 'Digite aqui...'}
-            placeholderTextColor={placeholderColors[index] || undefined}
-            icon={icons[index] || undefined}
-            iconColors={iconColors[index] || undefined}
-          />
-        ))}
+        {placeholders.map((placeholder, index) => {
+          const key = fieldKeys[index];
+          const value = key ? props.defaultValues?.[key] : '';
+          const defaultValue = value !== undefined ? String(value) : '';
+
+          return (
+            <Input
+              key={index}
+              placeholder={placeholder || 'Digite aqui...'}
+              placeholderTextColor={placeholderColors[index] || undefined}
+              icon={icons[index] || undefined}
+              iconColors={iconColors[index] || undefined}
+              defaultValue={defaultValue}
+            />
+          );
+        })}
       </View>
-      <View>{props.children}</View>
+
+      <View>{children}</View>
+
       <Button
         style={{ marginTop: 'auto', marginBottom: 12, alignSelf: 'center' }}
         title="Salvar"
