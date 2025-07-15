0fa83f2650525ea27db513c4cbe9b7ca61ddbe86
Jhonata Kornaker <jhonataalberto@outlook.com>
2025-07-15 20:37:13 -0300

    refactor: ajustar Screen de cadastro.

--- src/screens/CadastroFerramentas.tsx
+++ src/screens/CadastroFerramentas.tsx
@@ -1,12 +1,26 @@
 import ScreenCadastro from '@/components/ScreenCadastro';
+import { Ferramenta } from '@/types/ListaDeItens';
+import { MainStackParamList } from '@/types/MainStackNavigator';
+import { RouteProp, useRoute } from '@react-navigation/native';
 import { CircleAlert } from 'lucide-react-native';
 
+type CadastroFerramentasProps = RouteProp<
+  MainStackParamList,
+  'CadastroFerramentas'
+>;
+
 export default function CadastroFerramentas() {
+  const route =
+    useRoute<RouteProp<MainStackParamList, 'CadastroFerramentas'>>();
+  const ferramenta = route.params?.ferramenta;
+
   return (
-    <ScreenCadastro
+    <ScreenCadastro<Ferramenta>
       placeholders={['Nome da Ferramenta', 'Marca', 'Modelo', 'Quantidade']}
       icons={[CircleAlert, undefined, undefined, CircleAlert]}
       iconColors={['#FF001F80', '', '', '#FF001F80']}
-    ></ScreenCadastro>
+      defaultValues={ferramenta}
+      fieldKeys={['descricao', 'marca', 'modelo', 'quantidade']}
+    />
   );
 }
--- src/screens/CadastroPatrimonio.tsx
+++ src/screens/CadastroPatrimonio.tsx
@@ -2,16 +2,22 @@ import { Input } from '@/components/Input';
 import RadionComponent from '@/components/RadionComponent';
 import ScreenCadastro from '@/components/ScreenCadastro';
 import { theme } from '@/styles/theme';
+import { Patrimonio } from '@/types/ListaDeItens';
+import { MainStackParamList } from '@/types/MainStackNavigator';
+import { RouteProp, useRoute } from '@react-navigation/native';
 import { CircleAlert } from 'lucide-react-native';
 import { useState } from 'react';
 import { Text, View } from 'react-native';
 
 export default function CadastroPatrimonio() {
-  const [opcao, setOpcao] = useState('nao');
+  const route = useRoute<RouteProp<MainStackParamList, 'CadastroPatrimonio'>>();
+  const patrimonio = route.params?.patrimonio;
+  const [opcao, setOpcao] = useState(patrimonio ? 'sim' : 'nao');
+  const isEdicao = !!patrimonio;
   const [campoAdicional, setCampoAdicional] = useState('');
 
   return (
-    <ScreenCadastro
+    <ScreenCadastro<Patrimonio>
       placeholders={[
         'Nome do Equipamento',
         'Marca',
@@ -20,6 +26,15 @@ export default function CadastroPatrimonio() {
       ]}
       icons={[CircleAlert, undefined, undefined, CircleAlert]}
       iconColors={['#FF001F80', '', '', '#FF001F80']}
+      defaultValues={patrimonio}
+      fieldKeys={[
+        'numeroSerie',
+        'descricao',
+        'marca',
+        'modelo',
+        'locado',
+        'dataLocacao',
+      ]}
     >
       <View
         style={{
@@ -39,7 +54,11 @@ export default function CadastroPatrimonio() {
         >
           Equipamento Alugado ?
         </Text>
-        <RadionComponent value={opcao} onChange={setOpcao} />
+        <RadionComponent
+          value={opcao}
+          onChange={setOpcao}
+          disabled={isEdicao}
+        />
       </View>
 
       {opcao === 'sim' && (
@@ -49,12 +68,14 @@ export default function CadastroPatrimonio() {
             icon={CircleAlert}
             iconColors="#FF001F80"
             iconPosition="right"
+            defaultValue={patrimonio?.locado ? 'Locadora XPTO' : ''}
           />
           <Input
             placeholder="Data da Locação"
             icon={CircleAlert}
             iconColors="#FF001F80"
             iconPosition="right"
+            defaultValue={patrimonio?.dataLocacao ?? ''}
           />
         </View>
       )}
