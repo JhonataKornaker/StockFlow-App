53293388ed8b3a8f155175409144ab418b6c6080
Jhonata Kornaker <jhonataalberto@outlook.com>
2025-07-15 20:22:21 -0300

    refactor: adicionar Toast no App.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import 'react-native-gesture-handler';
 import 'react-native-reanimated';
 import AppNavigation from './src/navigation/AppNavigation';
+import Toast from 'react-native-toast-message';
 
 export default function App() {
-  return <AppNavigation />;
+  return (
+    <>
+      <AppNavigation />
+      <Toast />
+    </>
+  );
 }
